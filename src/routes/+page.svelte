d4a509afeefd0d67b44f2c38a6e635c0d3addb44
SantherEHPM <erickhernandop@gmail.com>
2024-08-07 12:25:54 -0500

    Deploy test 6

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,6 @@
 	import Counter from './Counter.svelte';
 	import welcome from '$lib/images/svelte-welcome.webp';
 	import welcome_fallback from '$lib/images/svelte-welcome.png';
-
-	console.log(import.meta.env.VITE_BASE_URL)
 </script>
 
 <svelte:head>
